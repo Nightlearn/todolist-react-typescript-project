0b371afa6b2ec57188c045a24f2f84fabd551b90
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-07-29 01:07:29 +0430

    add vazirmatn font

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import TodoList from './components/todoList';
 
 function App() {
   return (
-    <div>
-      <div className='flex bg-slate-50 shadow'>
+    <div dir='rtl'>
+      <div className='flex bg-slate-50 shadow justify-between'>
         <div className='p-1'>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
+        <div className='p-1 pl-3'>سلام مهندس خوبی ...</div>
       </div>
      <TodoList/>
     </div>
